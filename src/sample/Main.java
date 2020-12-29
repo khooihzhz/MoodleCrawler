a113477741c2f13908c1aef90e64ac7747e32a4f
Hayden Soong <soonghingfei@hotmail.com>
2020-12-29 11:59:37 +0800

    Fixed bugs

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,6 +16,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -25,9 +28,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("loginpage.fxml"));
+        primaryStage.setTitle("Moodle Crawler");
+        primaryStage.setScene(new Scene(root, 771, 505));
         primaryStage.show();
     }
 
@@ -258,8 +261,61 @@ public class Main extends Application {
         }
     }
 
+    static void arrangeFiles(String courseTitle) {
+        String[] fileList;
+        String downloadPath;
+        String folderPath;
+
+        // ELIMINATE SPECIAL CHARACTERS
+        courseTitle = courseTitle.replaceAll("[^a-zA-Z0-9&]", " ");
+
+        // STORE DOWNLOAD PATH
+        downloadPath = System.getProperty("user.dir") + "\\externalFiles\\downloadFiles";
+        File downloadFile = new File(downloadPath);
+
+        // PATH FOR NEW FOLDER
+        folderPath = System.getProperty("user.dir")+"\\externalFiles\\"+courseTitle;
+        File newFolder = new File(folderPath);
+        // CREATE DIRECTORY
+        newFolder.mkdir();
+
+        // STORE LIST OF FILE NAMES
+        fileList = downloadFile.list();
+
+        // STRING PATTERN FOR ASSIGNMENTS AND TUTORIALS
+        String assignPattern = "^.*[Cc]oursework.*$|^.*[Aa]ssignment.*$|^.*[Pp]roject.*$";
+        String tutorialPattern = "^.*[Tt]utorial.*$|^.*[Ll]ab.*$|^.*[Tt]ut.*$";
+        String path = "";
+        // MOVE FILES INTO THE NEW DIRECTORY
+        for (String filename:fileList) {
+            if (filename.matches(assignPattern)) {
+                // STORE ASSIGNMENT FOLDER PATH
+                path = folderPath+"\\Assignments";
+            }
+            else if (filename.matches(tutorialPattern)) {
+                // STORE TUTORIAL FOLDER PATH
+                path = folderPath+"\\Tutorials";
+            }
+            else {
+                // STORE LECTURES FOLDER PATH
+                path = folderPath+"\\Lectures";
+            }
+            // CREATE DIRECTORY
+            File f = new File(path);
+            f.mkdir();
+            // MOVE FILE TO THE TARGETED DIRECTORY
+            try {
+                Files.move(Paths.get(downloadPath+"\\"+filename), Paths.get(path+"\\"+filename));
+            }
+            catch (IOException e){
+                // do nothing
+            }
+        }
+    }
+
     public static void main(String[] args)
     {
+        //launch(args);
         getMoodleCookies();
 
         // === IMPLEMENT SELECTION FUNCTION HERE ====
